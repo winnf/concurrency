a40f1af4bdf882955fc1cfad8d1faef05db3465b
Winnie Fong <winniefong@Winnies-MacBook-Pro.local>
2017-03-25 18:09:15 -0400

    getting yield

--- Driver.java
+++ Driver.java
@@ -1,21 +1,24 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Driver {
+
+    static AtomicBoolean ready = new AtomicBoolean(false);
+
     public static void main(String args[]) {
         Thread myThreads[] = new Thread[300];
         Prime primeObj = new Prime();
-        AtomicBoolean ready = new AtomicBoolean(); //set atomic boolean to false
         int num = 654323;
 
         for (int i = 0; i < 299; i++){
-            myThreads[i] = new Thread(new RunnableThread(i, num));
+            myThreads[i] = new Thread(new RunnableThread(i, num, ready));
             myThreads[i].start();
-            ready.set(true); //wait until all threads are ready before running
         }
+        
+        System.out.println("READY");
+        ready.set(true); //wait until all threads are ready before running
         for (int i = 0; i < 299; i++){
             try {
                 myThreads[i].join();
-                System.out.println("Prime Number?: " + primeObj.getIsPrime()); 
             } catch (InterruptedException ie) {} 
         }
     }
