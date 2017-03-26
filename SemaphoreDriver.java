dd2c0bdb7ce3af189f8f679b268f0f7d505fff75
Winnie Fong <winniefong@Winnies-MacBook-Pro.local>
2017-03-26 15:57:16 -0400

    added basic counting semaphore

---
+++ SemaphoreDriver.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.Semaphore;
+import java.io.Console;
+
+public class SemaphoreDriver {
+    public static void main(String args[]){
+        Thread myThreads[] = new Thread[5]; //initialized with 5 threads for now
+        //int s = Integer.parseInt(str); //obtain # of semaphores from user input
+
+        final Semaphore sem = new Semaphore(2, true);
+
+        for (int i = 0; i < 4; i++){
+            myThreads[i] = new Thread(new SemaphoreThread(i, 2, sem));
+            myThreads[i].start();
+        }
+        for (int i = 0; i < 4; i++){
+            try {
+                myThreads[i].join();
+            } catch (InterruptedException ie) {} 
+        }
+    } 
+}
