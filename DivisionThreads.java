4ba2fa1d52481af8f6026fc079bab2af48b28870
ANNA DONG <annarhdong@gmail.com>
2017-03-26 00:14:44 -0400

    exercise 2
    didn’t run it yet dunno if it’ll work yet

---
+++ DivisionThreads.java
@@ -0,0 +1,42 @@
+//java thread creation
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DivisionThread implements Runnable {
+    private int threadID;
+    private int targetNumber;
+    private int start;
+    private int end;
+    private AtomicBoolean ready;
+    private Boolean isPrime = true;
+    private Lock lock = new Lock();
+
+    public DivisionThread(int threadID, int targetNumber, int start, int end, AtomicBoolean ready){
+        this.threadID = threadID;
+        this.targetNumber = targetNumber;
+        this.start = start;
+        this.end = end;
+        this.ready = ready;
+    }
+    public void run() {
+        while (!ready.get()){
+            Thread.yield(); //only run when atomic boolean is true
+        }
+
+        for(int i = this.start; i<= this.end; i++)
+        {
+          if(this.targetNumber % i == 0){
+            isPrime = false;
+            lock.lock();
+            System.out.println(i + " divides " + this.targetNumber);
+            lock.unlock();
+          }
+        }
+        lock.lock();
+        System.out.println("Thread "+ this.threadID + "is done");
+        lock.unlock();
+    }
+}
---
+++ exercise2.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Driver {
+
+    static AtomicBoolean ready = new AtomicBoolean(false);
+
+    public static void main(String args[]) {
+        Thread myThreads[] = new Thread[10];
+        int num = 5345;
+        int start = 2;
+        int end = start + (num-2)/10;
+
+        for (int i = 0; i < 9; i++){
+            myThreads[i] = new Thread(new DivisionThread(i, num, start, end, ready));
+            myThreads[i].start();
+            start = end;
+            end = start + (num-2)/10;
+        }
+
+        System.out.println("READY");
+        ready.set(true); //wait until all threads are ready before running
+        for (int i = 0; i < 10; i++){
+            try {
+                myThreads[i].join();
+            } catch (InterruptedException ie) {}
+        }
+
+        if(DivisionThread.isPrime == true)
+        {
+          System.out.println(num + " is a prime number");
+        }
+        else{
+          System.out.println(num + " is not a prime number");
+        }
+    }
+}
