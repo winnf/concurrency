ce9c43a021d192be0ef7529bef9054706e73cce9
Winnie Fong <winniefong@Winnies-MacBook-Pro.local>
2017-03-27 00:14:27 -0400

    take user input

--- SemaphoreDriver.java
+++ SemaphoreDriver.java
@@ -1,17 +1,22 @@
 import java.io.Console;
+import java.util.*;
 
 public class SemaphoreDriver {
     public static void main(String args[]){
-        Thread myThreads[] = new Thread[5]; //initialized with 5 threads for now
-        //int s = Integer.parseInt(str); //obtain # of semaphores from user input
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many threads?: ");
+        int numThreads = in.nextInt();
+        System.out.println("How many semaphores? (Value must be less than # of threads): ");
+        int numSem = in.nextInt();
+        Thread myThreads[] = new Thread[numThreads];
 
-        final Semaphore sem = new Semaphore(2);
+        final Semaphore sem = new Semaphore(numSem);
 
-        for (int i = 0; i < 4; i++){
-            myThreads[i] = new Thread(new SemaphoreThread(i, 2, sem));
+        for (int i = 0; i < numSem-1; i++){
+            myThreads[i] = new Thread(new SemaphoreThread(i, numSem, sem));
             myThreads[i].start();
         }
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < numSem-1; i++){
             try {
                 myThreads[i].join();
             } catch (InterruptedException ie) {} 
